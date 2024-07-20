3e1a2300ac36e9d452eb0eeccc2bc0a051509959
Vishnu Nanilal Panicker <vishnunlal@gmail.com>
2024-07-20 18:16:34 +0530

    Removed baseUrl from axios Instance creation.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001/",
     headers: {
         "Content-Type": 'application/json',
         Authorization: `Bearer ${localStorage.getItem('jwt')}`
